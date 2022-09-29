ee46ee8f36846487d6f2768a290fa770f1e8aa58
tyler <ttang79102bths.edu>
2022-09-29 12:32:43 -0400

    added Student class

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+
+//Instance variables
+    private String firstName;
+    private String lastName;
+    private int gradYear;
+    private double accumulatedTestScores;
+    private int testScoreCount;
+}
